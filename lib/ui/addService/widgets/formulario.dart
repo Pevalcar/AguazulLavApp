5725599c12bb36bd699eca3f53120711091a9d12
pevalcar <perroaa94@gmail.com>
2023-12-22 23:37:11 -0500

    agregar mas cosa

--- lib/ui/addService/widgets/formulario.dart
+++ lib/ui/addService/widgets/formulario.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:aguazullavapp/lib.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -16,33 +18,6 @@ class Formulario extends HookConsumerWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           ClientSearcher(),
-          TextFormField(
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            initialValue:
-                ref.watch(vehiculoStateProvider).value?.propietario.name ?? "",
-            onChanged: (value) {
-              ref.read(vehiculoStateProvider.notifier).modifyPropietario(value);
-            },
-            decoration: InputDecoration(
-              suffixIcon: IconButton(
-                  onPressed: () {
-                    //TODO search propietario
-                  },
-                  icon: Icon(
-                    Icons.person_search,
-                  )),
-              labelText: "Propietario",
-              icon: Icon(Icons.person),
-            ),
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Campo requerido';
-              } else if (value.length < 3) {
-                return 'Nombre demasiado corto';
-              }
-              return null;
-            },
-          ),
           TextFormField(
             autovalidateMode: AutovalidateMode.onUserInteraction,
             initialValue: ref.watch(vehiculoStateProvider).value?.placa,
@@ -69,19 +44,19 @@ class Formulario extends HookConsumerWidget {
   }
 }
 
-class ClientSearcher extends StatelessWidget {
+class ClientSearcher extends HookWidget {
   const ClientSearcher({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
+    //TODO add client implementacion 
+    final querry = useState('');
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
-        
         children: [
-      
           Flexible(
             child: SearchAnchor(
               builder: (context, controller) {
@@ -91,6 +66,7 @@ class ClientSearcher extends StatelessWidget {
                     controller.openView();
                   },
                   onChanged: (value) {
+                    querry.value = value;
                     controller.openView();
                   },
                   hintText: "Buscar propietario",
@@ -99,12 +75,11 @@ class ClientSearcher extends StatelessWidget {
               },
               suggestionsBuilder: (context, controller) {
                 return List<ListTile>.generate(5, (int index) {
-                  
                   final String item = 'item $index';
                   return ListTile(
                     title: Text(item),
                     onTap: () {
-                      controller.closeView( item);
+                      controller.closeView(item);
                     },
                   );
                 });
