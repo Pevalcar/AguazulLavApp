a39967b10721dc4275695ba839a3064d96a9a0eb
pevalcar <perroaa94@gmail.com>
2024-01-13 14:19:49 -0500

    algi

--- lib/features/services_list/presentation/providers/jornadaState/jornada_state_provider.dart
+++ lib/features/services_list/presentation/providers/jornadaState/jornada_state_provider.dart
@@ -12,11 +12,26 @@ class JornadaState extends _$JornadaState {
     return null;
   }
 
-  void iniciarJornada(int cajaicial) {
-    state = AsyncValue.data(Jornada(
-      id: const Uuid().v4(),
-      dateInit: DateTime.now(),
-      cajaInicial: cajaicial,
-    ));
+  void iniciarJornada(int cajaicial) async {
+    state = AsyncValue.loading();
+
+    state = await AsyncValue.guard(() async {
+      final jorndadInit = Jornada(
+        id: const Uuid().v4(),
+        dateInit: DateTime.now(),
+        cajaInicial: cajaicial,
+      );
+      return jorndadInit;
+    });
+  }
+
+  void finalizarJornada() async {
+    state = AsyncValue.loading();
+
+    state = await AsyncValue.guard(() async {
+      final Jornada? jorndadInit = state.value?.copyWith(dateEnd: DateTime.now());
+      
+      return jorndadInit;
+    });
   }
 }
