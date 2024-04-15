9868f9c86f608390905fda6c171f8a96edd0bc29
Pevalcar <42533773+Pevalcar@users.noreply.github.com>
2024-04-14 20:49:53 -0500

    Adding And modifie Calendar mode and show more beutifull

--- lib/mains/app.dart
+++ lib/mains/app.dart
@@ -2,6 +2,8 @@ import 'package:aguazullavapp/lib.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:syncfusion_localizations/syncfusion_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class MyApp extends HookConsumerWidget {
   const MyApp({super.key});
@@ -11,9 +13,23 @@ class MyApp extends HookConsumerWidget {
     final themeMode = ref.watch(dartModeProvider);
     final navigatorKey = ref.watch(enrutadorProvider);
     return MaterialApp.router(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        SfGlobalLocalizations.delegate
+      ],
+      supportedLocales: const [
+        Locale('es'),
+      ],
+      locale: const Locale('es'),
       title: EnvInfo.appName,
-      theme: MaterialTheme(GoogleFonts.robotoTextTheme(Theme.of(context).textTheme)).light(),
-      darkTheme: MaterialTheme(GoogleFonts.robotoTextTheme(Theme.of(context).textTheme)).dark(),
+      theme: MaterialTheme(
+              GoogleFonts.robotoTextTheme(Theme.of(context).textTheme))
+          .light(),
+      darkTheme: MaterialTheme(
+              GoogleFonts.robotoTextTheme(Theme.of(context).textTheme))
+          .dark(),
       themeMode: themeMode,
       debugShowCheckedModeBanner: false,
       routerConfig: navigatorKey,
